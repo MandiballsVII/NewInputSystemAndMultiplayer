fe7f1ebbee07b13f3878b346fb1c43b7234f5e09
MandiballsVII <149199591+MandiballsVII@users.noreply.github.com>
2025-01-23 16:53:32 +0100

    Movimientos básicos

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private float horizontal;
+    private float vertical;
+
+    private float horizontalSpeed = 2f;
+    private float verticalSpeed = 0.05f;
+    private float jumpForce = 16f;
+    private bool isFacingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontal * horizontalSpeed, rb.velocity.y + vertical * verticalSpeed);
+
+        if(!isFacingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        if(context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+        vertical = context.ReadValue<Vector2>().y;
+    }
+}
