8cbe244698cf881d396769648e53ed766618ca7c
MandiballsVII <149199591+MandiballsVII@users.noreply.github.com>
2025-01-24 10:44:01 +0100

    Basic player inputs

--- Assets/PlayerAim.cs
+++ Assets/PlayerAim.cs
@@ -6,87 +6,93 @@ using UnityEngine.InputSystem;
 
 public class PlayerAim : MonoBehaviour
 {
-    private Transform aimTransform;
-    private Vector3 aimlocalScale = Vector3.one;
+    //private Transform aimTransform;
+    //private Vector3 aimlocalScale = Vector3.one;
 
-    public Vector2 PointerPosition { get; set; }
+    //[SerializeField] private InputActionReference move, pointerPosition;
 
-    [SerializeField] private InputActionReference move, pointerPosition;
+    //void Awake()
+    //{
+    //    aimTransform = transform.Find("Aim");
+    //}
+    //private void Start()
+    //{
 
-    void Awake()
+    //}
+
+    //void Update()
+    //{
+    //    //transform.right = (PointerPosition - (Vector2)transform.position).normalized;
+    //    aimTransform.localScale = aimlocalScale;
+    //    //GetMouseWorldPosition();
+    //    //HandleAiming();
+    //    //print(Mouse.current.position.ReadValue());
+    //}
+
+    //// Get mouse position in world with Z = 0f.
+
+    //// Para comprobar boton de mouse apretado --> Mouse.current.leftButton.wasPressedThisFrame
+    //#region
+    //public static Vector3 GetMouseWorldPosition()
+    //{
+    //    //if (Mouse.current.leftButton.wasPressedThisFrame)
+    //    //{
+    //    //    print("Hola");
+    //    //    print(Mouse.current.position.ReadValue());
+    //    //}
+    //    Vector3 mousePos = Mouse.current.position.ReadValue();
+    //    mousePos.z = Camera.main.nearClipPlane;
+    //    return Camera.main.ScreenToWorldPoint(mousePos);
+    //    //Vector3 vec = GetMouseWorldPositionWithZ(Mouse.current.position.ReadValue(), Camera.main);
+    //    //vec.z = 0f;
+    //    //return vec;
+    //}
+    //public static Vector3 GetMouseWorldPositionWithZ()
+    //{
+    //    return GetMouseWorldPositionWithZ(Mouse.current.position.ReadValue(), Camera.main);
+    //}
+    //public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    //{
+    //    return GetMouseWorldPositionWithZ(Mouse.current.position.ReadValue(), worldCamera);
+
+    //}
+    //public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    //{
+    //    Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+    //    return worldPosition;
+    //}
+    //#endregion
+
+    //public void HandleAiming(InputAction.CallbackContext context)
+    //{
+    //    Vector3 mousePosition = GetMouseWorldPosition();
+
+    //    Vector3 aimDirection = (mousePosition - transform.position).normalized;
+    //    print(aimDirection);
+    //    float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+    //    aimTransform.eulerAngles = new Vector3(0, 0, angle);
+
+    //    if (context.ReadValue<Vector2>().x != 0 || context.ReadValue<Vector2>().y != 0)
+    //    {
+    //        angle = Mathf.Atan2(context.ReadValue<Vector2>().y, context.ReadValue<Vector2>().x) * Mathf.Rad2Deg;
+    //        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    //    }
+    //    //UnityEngine.Debug.Log(angle);
+    //    if (angle > 90 || angle < -90)
+    //    {
+    //        aimlocalScale.y = -1f;
+    //    }
+    //    else
+    //    {
+    //        aimlocalScale.y = +1f;
+    //    }
+    //}
+
+    public Vector2 MousePosition { get; set; }
+
+    private void Update()
     {
-        aimTransform = transform.Find("Aim");
+        transform.right = (MousePosition - (Vector2)transform.position).normalized;
+        //print(MousePosition);
     }
-    private void Start()
-    {
-
-    }
-
-    void Update()
-    {        
-        //transform.right = (PointerPosition - (Vector2)transform.position).normalized;
-        aimTransform.localScale = aimlocalScale;
-        //GetMouseWorldPosition();
-        //HandleAiming();
-        print(Mouse.current.position.ReadValue());
-    }
-
-    // Get mouse position in world with Z = 0f.
-
-    // Para comprobar boton de mouse apretado --> Mouse.current.leftButton.wasPressedThisFrame
-    #region
-    public static Vector3 GetMouseWorldPosition()
-    {
-        //if (Mouse.current.leftButton.wasPressedThisFrame)
-        //{
-        //    print("Hola");
-        //    print(Mouse.current.position.ReadValue());
-        //}
-        Vector3 mousePos = Mouse.current.position.ReadValue();
-        mousePos.z = Camera.main.nearClipPlane;
-        return Camera.main.ScreenToWorldPoint(mousePos);
-        //Vector3 vec = GetMouseWorldPositionWithZ(Mouse.current.position.ReadValue(), Camera.main);
-        //vec.z = 0f;
-        //return vec;
-    }
-    public static Vector3 GetMouseWorldPositionWithZ()
-    {
-        return GetMouseWorldPositionWithZ(Mouse.current.position.ReadValue(), Camera.main);
-    }
-    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
-    {
-        return GetMouseWorldPositionWithZ(Mouse.current.position.ReadValue(), worldCamera);
-
-    }
-    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
-    {
-        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
-        return worldPosition;
-    }
-    #endregion
-
-    public void HandleAiming(InputAction.CallbackContext context)
-    {
-        Vector3 mousePosition = GetMouseWorldPosition();
-
-        Vector3 aimDirection = (mousePosition - transform.position).normalized;
-        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-        aimTransform.eulerAngles = new Vector3(0, 0, angle);
-
-        if (context.ReadValue<Vector2>().x != 0 || context.ReadValue<Vector2>().y != 0)
-        {
-            angle = Mathf.Atan2(context.ReadValue<Vector2>().y, context.ReadValue<Vector2>().x) * Mathf.Rad2Deg;
-            aimTransform.eulerAngles = new Vector3(0, 0, angle);
-        }
-        //UnityEngine.Debug.Log(angle);
-        if (angle > 90 || angle < -90)
-        {
-            aimlocalScale.y = -1f;
-        }
-        else
-        {
-            aimlocalScale.y = +1f;
-        }
-    }
-
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -18,6 +20,21 @@ public class PlayerMovement : MonoBehaviour
     private float jumpForce = 16f;
     private bool isFacingRight = true;
 
+    [SerializeField] private InputActionReference movement, attack, mousePosition, rightStickInput;
+
+    private Vector2 pointerInput;
+    private Vector2 movementInput;
+
+    private Vector3 aimlocalScale = Vector3.one;
+
+    //private PlayerAim playerAim;
+
+    public Transform playerAim;
+
+    private void Awake()
+    {
+        ///playerAim = GetComponentInChildren<PlayerAim>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -27,16 +44,52 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(horizontal * horizontalSpeed, rb.velocity.y + vertical * verticalSpeed);
+        //rb.velocity = new Vector2(horizontal * horizontalSpeed, rb.velocity.y + vertical * verticalSpeed);
+        rb.velocity = movementInput * horizontalSpeed;
 
-        if(!isFacingRight && horizontal > 0f)
+        if (!isFacingRight && movementInput.x > 0f)
         {
             Flip();
         }
-        else if (isFacingRight && horizontal < 0f)
+        else if (isFacingRight && movementInput.x < 0f)
         {
             Flip();
         }
+        movementInput = movement.action.ReadValue<Vector2>();
+
+        float angle = Mathf.Atan2(playerAim.transform.right.y, playerAim.transform.right.x) * Mathf.Rad2Deg;
+        if (rightStickInput.action.ReadValue<Vector2>() != Vector2.zero)
+        {
+            angle = Mathf.Atan2(GetRightStickInput().y, GetRightStickInput().x) * Mathf.Rad2Deg;
+            playerAim.eulerAngles = new Vector3(0, 0, angle);
+        }
+        pointerInput = GetPointerInput();
+        playerAim.GetComponent<PlayerAim>().MousePosition = pointerInput;
+        //playerAim.MousePosition = pointerInput;
+        //print(playerAim.MousePosition);
+
+        if (angle > 90 || angle < -90)
+        {
+            aimlocalScale.y = -1f;
+        }
+        else
+        {
+            aimlocalScale.y = +1f;
+        }
+    
+        playerAim.transform.localScale = aimlocalScale;
+    }
+
+    private Vector2 GetPointerInput()
+    {
+        Vector3 mousePos = mousePosition.action.ReadValue<Vector2>();
+        mousePos.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    private Vector2 GetRightStickInput()
+    {
+        return rightStickInput.action.ReadValue<Vector2>();
     }
 
     public void Jump(InputAction.CallbackContext context)
